58ed6593990b8f31f69431ea325983ff9a025ca4
Aman07a <aman16.aa17@gmail.com>
2024-01-22 15:42:23 +0100

    Host Element Selectors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,5 +27,11 @@ export class AppComponent {
       username: 'biking12222',
       content: 'I did some biking today',
     },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking12222',
+      content: 'I did some biking today',
+    },
   ];
 }
